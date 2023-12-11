694e0b1e0bcb650802ef69c12a3cbe0c882c699d
Kyusung Sohn <rok.ksohn@gmail.com>
2023-12-11 12:49:40 +0900

    Feat: set up initial UI layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,20 @@ import { InvestmentInput } from "@/components/investment";
 
 export default function Home() {
   return (
-    <div className="debug-border h-screen p-10 flex flex-col gap-5">
-      <h1>Investment Portfolio Calculator</h1>
+    <div className="h-screen p-10 flex flex-col gap-5">
+      <h1 className="font-bold text-gray-700 text-2xl">투자 포트폴리오 계산기</h1>
       <div className="h-full flex flex-col border rounded-md border-gray-300">
-        <div className="h-[50px] w-full bg-primary text-white font-bold">LABELS</div>
+        <div className="rounded-t-md flex h-[50px] w-full bg-primary text-white font-bold">
+          <p className="w-[80px] h-full flex justify-center items-center">구분</p>
+          <div className="flex w-full justify-between">
+            <p className="w-full h-full flex justify-center items-center">자산군</p>
+            <p className="w-full h-full flex justify-center items-center">현재 가격</p>
+            <p className="w-full h-full flex justify-center items-center">보유 개수</p>
+            <p className="w-full h-full flex justify-center items-center">현재 비율</p>
+            <p className="w-full h-full flex justify-center items-center">성장형</p>
+            <p className="w-full h-full flex justify-center items-center">안전형</p>
+          </div>
+        </div>
         <div className="h-full flex">
           <div className="grid grid-rows-[4fr,1fr,3fr,2fr] w-[80px] h-full text-sm text-gray-500 font-bold">
             <p className="h-full flex justify-center items-center border-r border-b border-gray-300">주식</p>
@@ -14,7 +24,7 @@ export default function Home() {
             <p className="h-full flex justify-center items-center border-r border-b border-gray-300">국채</p>
             <p className="h-full flex justify-center items-center border-r border-gray-300">현금성자산</p>
           </div>
-          <div className="flex flex-col h-full justify-around">
+          <div className="w-full flex flex-col h-full justify-around">
             <InvestmentInput mainLabel="미국 주식 (UH)" subLabel="(KODEX 미국 S&P500TR)" />
             <InvestmentInput mainLabel="한국 주식" subLabel="(KOSEF 200TR)" />
             <InvestmentInput mainLabel="중국 주식" subLabel="(KODEX 차이나 CSI300)" />
@@ -27,7 +37,13 @@ export default function Home() {
             <InvestmentInput mainLabel="예수금" subLabel="(총액에는 합계되지 않음 *)" />
           </div>
         </div>
-        <div className="border-t border-gray-300">hi!</div>
+        <div className="pl-[90px] h-[50px] border-t border-gray-300 grid grid-cols-[2fr,1fr,1fr,1fr,1fr] text-lg font-bold text-gray-500">
+          <p className="w-full h-full flex justify-center items-center">합 계</p>
+          <p className="w-full h-full flex justify-center items-center">1020</p>
+          <p className="w-full h-full flex justify-center items-center">100.0%</p>
+          <p className="w-full h-full flex justify-center items-center">100.0%</p>
+          <p className="w-full h-full flex justify-center items-center">100.0%</p>
+        </div>
       </div>
     </div>
   );
--- src/components/investment/InvestmentInput.tsx
+++ src/components/investment/InvestmentInput.tsx
@@ -5,13 +5,16 @@ interface InvestmentInputProps {
 
 export default function InvestmentInput({ mainLabel, subLabel = "" }: InvestmentInputProps) {
   return (
-    <div className="flex gap-5">
-      <div className="w-[250px] flex flex-col items-center gap-1 h-[40px] justify-center">
+    <div className="flex gap-5 w-full justify-around">
+      <div className="w-full flex flex-col items-center gap-1 h-[40px] justify-center">
         <p className="font-bold">{mainLabel}</p>
         <p className="text-xs">{subLabel}</p>
       </div>
       <input type="number" className="input"></input>
       <input type="number" className="input"></input>
+      <p className="w-full text-center font-bold text-lg text-primary">50%</p>
+      <p className="w-full text-center font-bold text-lg text-primary">50%</p>
+      <p className="w-full text-center font-bold text-lg text-primary">50%</p>
     </div>
   );
 }
